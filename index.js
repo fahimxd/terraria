a0b0beca94e4a32751fea2ed88a6e4c7b225aaea
fahimxd <82989068+fahimxd@users.noreply.github.com>
2022-05-08 11:28:01 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,200 @@
+function a1(){
+    a=document.getElementById("s1").value
+    console.log(a)
+    if(a=="weapon"){
+        window.open("page2.html","_self");
+    }else if(a== "potion"){
+        window.open("potion.html","_self");
+    }
+    else if(a=="home"){
+        window.open("index.html","_self")
+    }
+    else {
+        document.getElementById("w3").innerHTML="you already are in home page";
+    }
+}
+function cng()
+{
+    
+var e = document.getElementById("p1");
+if(e.id!=null || e.id=="p1"){
+    e.id = "p2";
+}
+
+}
+function d()
+{
+   
+    var e = document.body;
+    if(e.classList=="l"){
+        e.classList = "d";
+    }else if(e.classList=="d"){
+        e.classList = "l";
+    }
+  
+}
+function ss1()
+{
+    a = document.getElementById("c1a").innerHTML="The  is a Hardmode, post-Moon Lord melee weapon crafted using a variety of swords obtained throughout the game's progression. When swung, the sword sprites of its component swords fly towards enemies within a 20-tile radius of the cursor (regardless of where the cursor is on the screen), and circle back in quick, frenzied arcs. They pass through solid blocks, and three swords will appear per use (at base melee speed). Each sword sprite deals the weapon's full damage.";
+    i = document.getElementById("c2a").src="image/Zenith.png";
+    l = document.getElementById("li").href="http://www.terraria.fandom.com/wiki/Zenith";
+}
+function ss2()
+{
+    a = document.getElementById("c1a").innerHTML="The Terra Blade is a Hardmode projectile sword. When used, it fires a green sword-shaped projectile which costs neither mana nor ammunition, deals  50% /  25% more damage than the sword, and can pierce through two enemies, dissipating after hitting a third enemy or a wall. The projectile can travel up to 5000 feet (2500 tiles) at a speed of 61 mph.";
+    i = document.getElementById("c2a").src="image/Terra_Blade.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Terra_Blade";
+
+}
+function ss3()
+{
+    a = document.getElementById("c1a").innerHTML="The Meowmere is a Hardmode, post-Moon Lord, autoswinging melee weapon. When swung, it fires a projectile trailed by a rainbow and shaped like a cat's head, and consumes no mana or ammunition. The projectile is affected by gravity, pierces multiple enemies, and ricochets off of blocks. Each ricochet emits an explosion that damages nearby enemies. The projectile disappears after piercing four enemies (hitting the fifth one) or after ricocheting four times (hitting a block for the fifth time). The Meowmere has a 1/9 (11.11%) chance to be dropped by the Moon Lord.";
+    i = document.getElementById("c2a").src="image/Meowmere.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Meowmere";
+}
+function ss4()
+{
+    a = document.getElementById("c1a").innerHTML="The Star Wrath is a Hardmode, post-Moon Lord sword which causes three pink projectiles to fall at the cursor whenever it is swung, each dealing double the sword's damage. It can be considered an upgrade of the Starfury. The Star Wrath has 1/9 (11.11%) chance to be dropped by the Moon Lord.";
+    i = document.getElementById("c2a").src="image/Star_Wrath.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Star_Wrath";
+
+}
+function ss5()
+{
+    a = document.getElementById("c1a").innerHTML="The Influx Waver is a Hardmode, post-Golem broadsword. It fires a sword projectile in a straight line towards the cursor as it is swung. After the projectile successfully makes contact with an enemy or another hostile projectile, it will disappear, but can return up to two more times to attack a nearby enemy, or the same enemy.";
+    i = document.getElementById("c2a").src="image/Influx_Waver.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Influx_Waver";
+
+}
+function ss6()
+{
+    a = document.getElementById("c1a").innerHTML="The Horseman's Blade is a Hardmode, post-Plantera sword which summons homing pumpkin heads upon hitting an enemy. Despite it being called Horseman's Blade, it drops from Pumpking, a boss from the Pumpkin Moon event, not the Headless Horseman. It has a 1.79 - 14.29% chance of dropping from the Pumpking.";
+    i = document.getElementById("c2a").src="image/The_Horseman.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/The_Horseman%27s_Blade";
+
+}
+function ss7()
+{
+    a = document.getElementById("c1a").innerHTML="The Seedler is a Hardmode sword that has a 1/7 (14.29%) / 2/7 (28.57%) chance to be dropped by Plantera. It fires out grenade-like projectiles, which explode after a period of time (0.5-1 second) or when hitting an enemy, sending out 4 to 7, smaller and faster shards.";
+    i = document.getElementById("c2a").src="image/Seedler.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Seedler";
+
+}
+function ss8()
+{
+    a = document.getElementById("c1a").innerHTML="The Starfury is a sword that summons a star projectile from the sky when swung. The star falls to the location of the mouse cursor upon use, emitting light particles and piercing up to two enemies, but dealing no knockback. The Starfury can only be obtained from Skyware Chests,";
+    i = document.getElementById("c2a").src="image/Starfury.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Starfury";
+
+}
+function ss9()
+{
+    a = document.getElementById("c1a").innerHTML="The Bee Keeper is a pre-Hardmode broadsword that has a 33% chance to drop from the Queen Bee. Upon attacking an enemy, it summons friendly Bees (1-3, or 1-4 with a Hive Pack equipped), and has a 90% chance of inflicting the Confused debuff on the struck enemy.";
+    i = document.getElementById("c2a").src="image/Bee_Keeper.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Bee_Keeper";
+
+}
+function ss10()
+{
+    a = document.getElementById("c1a").innerHTML="The Enchanted Sword is a pre-Hardmode, autoswinging projectile sword. When swung, it fires a sword-shaped projectile that costs no ammunition or mana. Both the sword and its projectiles produce lingering sparks that emit large amounts of light. The projectile can be fired once every second, and dissipates upon hitting an enemy or solid block.";
+    i = document.getElementById("c2a").src="image/Enchanted_Sword.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Enchanted_Sword_(item)";
+
+}
+function ss11()
+{
+    a = document.getElementById("c1a").innerHTML="The Copper Shortsword is the weakest of the shortswords. It has a slightly stronger counterpart, the Tin Shortsword, except in the Nintendo Nintendo 3DS version version, where the Copper Shortsword is equivalent.";
+    i = document.getElementById("c2a").src="image/Copper_Shortsword.png";
+    l = document.getElementById("li").href="https://terraria.fandom.com/wiki/Copper_Shortsword";
+
+}
+/*function ib()
+{
+    b = document.getElementById("c1").style.left = "0%";
+
+    setTimeout(function()
+{
+    b = document.getElementById("c1").style.left = "-100%";
+}, 9000);
+}*/
+
+
+function ib(){
+    document.getElementById("c1").style.left = "0%";
+    ib1();
+}
+
+var myTimeout;
+
+function ib1(){
+    myTimeout = setTimeout(myGreeting, 5000);
+
+
+    function myGreeting() {
+        clearTimeout(myTimeout);
+        document.getElementById("c1").style.left = "-100%";
+        
+    }
+}  
+function potov(){
+    document.getElementById("pot").style.backgroundColor="rgba(85, 103, 206, 0)";
+}  
+function potou(){
+    document.getElementById("pot").style.backgroundColor=("");
+}
+var bb1 = document.getElementById("bims1");
+var bb2 = document.getElementById("bims2");
+var bb3 = document.getElementById("bims3");
+var bb4 = document.getElementById("bims4");
+var bbac= document.getElementById("bisbac");
+var bbac1= document.getElementById("bisbac1");
+var fog = document.getElementById("fogs");
+function defop(){
+    bb2.style.visibility="visible";
+    bb3.style.visibility="visible";
+    bb4.style.visibility="visible";
+    bbac.style.transform="scale(1.3)"
+    bbac1.style.visibility="visible";
+    bbac1.style.transform="scale(1.3)"
+    bbac1.style.opacity="0.7";
+    fog.style.opacity="70%";
+}
+function defou(){
+    bb2.style.visibility="";
+    bb3.style.visibility="";
+    bb4.style.visibility="";
+    bbac.style.transform="scale(1)";
+    bbac1.style.transform="scale(1)";
+    bbac1.style.opacity="0";
+    fog.style.opacity="0";
+}
+function desert(){
+    bb1.src="image/maps/desert_lanscape.png";
+    bb2.src="image/maps/Desert_Key.png";
+    bbac.src="image/maps/Desert_background.jpg";
+    bb3.src="image/maps/Antlion_Swarmer.png";
+    bb4.src="image/maps/Vulture.png";
+    bbac1.src="image/maps/sandstorm.png";
+    bb2.style.bottom= "137px";
+    bb3.style.left= "400px";
+    bb4.style.bottom= "131px";
+    fog.style.background="radial-gradient(circle at center, rgba(255, 0, 0, 0) 0, rgba(70, 54, 25, 0.329), rgb(90, 65, 27) 100%)"; 
+}
+function forest(){  
+    bb1.src="image/maps/forest_lanscape.png";
+    bb2.src="image/maps/Living_Wood_Wand.png";
+    bb3.src="image/maps/Demon_Eye.png";
+    bb4.src="image/maps/Zombie.png";
+    bbac.src="image/maps/forest_background.png";
+    bbac1.src="";
+    bb2.style.bottom= "163px";
+    bb3.style.bottom= "270px";
+    bb4.style.bottom= "158px";
+
+
+    fog.style.background= "radial-gradient(circle at center, rgba(255, 0, 0, 0) 0, rgba(76, 76, 194, 0.329), rgba(77, 79, 190, 0.849) 100%);";
+
+}
+function snow(){
+    /* */
+}
